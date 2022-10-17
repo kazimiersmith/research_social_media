e10ac8caa720ee2f5521568759eebf51bbe94964
kas1112 <kas1112@stern.nyu.edu>
2022-10-17 15:15:39 -0400

    Code to estimate transition function

---
+++ code/r/reg_follower_change.R
@@ -0,0 +1,9 @@
+data <- 'C:/Users/kas1112/Documents/research_social_media/data'
+out <- paste0(data, '/out')
+reg_data <- paste0(out, '/estimation')
+
+df_ig = read.csv(paste0(reg_data, '/transition_estimation_data.csv'))
+
+model <- lm(log_followers_next ~ log_frac_spon + log_engagement, data = df_ig)
+
+model
