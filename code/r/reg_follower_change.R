1200c86d301508d28f955a1530ecbeba233a573c
kas1112 <kas1112@stern.nyu.edu>
2022-10-28 17:35:06 -0400

    Add more regressions for follower equation

--- code/r/reg_follower_change.R
+++ code/r/reg_follower_change.R
@@ -1,3 +1,5 @@
+library(stargazer)
+
 data <- 'C:/Users/kas1112/Documents/research_social_media/data'
 out <- paste0(data, '/out')
 reg_data <- paste0(out, '/estimation')
@@ -5,5 +7,20 @@ reg_data <- paste0(out, '/estimation')
 df_ig = read.csv(paste0(reg_data, '/transition_estimation_data.csv'))
 
 model <- lm(change_log_followers ~ log_frac_spon + log_engagement, data = df_ig)
+summary(model)
 
-model
+model2 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts, data = df_ig)
+summary(model2)
+
+model3 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts + log_likes, data = df_ig)
+summary(model3)
+
+model4 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts + log_comments, data = df_ig)
+summary(model4)
+
+model5 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts + log_likes + log_comments, data = df_ig)
+summary(model5)
+
+stargazer(model, model2, model3, model4, model5, out = 'results.tex')
+
+          
