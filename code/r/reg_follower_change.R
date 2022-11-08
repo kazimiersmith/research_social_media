704b6688f7df594d7aa92470c44ca47d94d25fb4
kas1112 <kas1112@stern.nyu.edu>
2022-11-08 14:12:52 -0500

    Follower growth regression using the population growth model

--- code/r/reg_follower_change.R
+++ code/r/reg_follower_change.R
@@ -1,26 +1,27 @@
 library(stargazer)
 
-data <- 'C:/Users/kas1112/Documents/research_social_media/data'
+root <- 'C:/Users/kas1112/Documents/research_social_media'
+data <- paste0(root, '/data')
 out <- paste0(data, '/out')
 reg_data <- paste0(out, '/estimation')
+tab <- paste0(root, '/tab')
 
-df_ig = read.csv(paste0(reg_data, '/transition_estimation_data.csv'))
+# Carrying capacity for follower growth model
+carrying_cap <- 3500000
 
-model <- lm(change_log_followers ~ log_frac_spon + log_engagement, data = df_ig)
+df_ig <- read.csv(paste0(reg_data, '/posts_panel.csv'))
+
+# Terms in the follower growth regression
+df_ig <- transform(df_ig,
+  const_term = followers * (1 - followers / carrying_cap),
+  spon_term = sponsored_posts * followers * (1 - followers / carrying_cap),
+  posts_term = posts * followers * (1 - followers / carrying_cap),
+  eng_term = engagement * followers * (1 - followers / carrying_cap)
+)
+
+write.csv(df_ig, paste0(reg_data, '/posts_panel_regression_test.csv'))
+
+model1 <- lm(change_followers ~ const_term + spon_term + posts_term + eng_term, data = df_ig)
 summary(model)
 
-model2 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts, data = df_ig)
-summary(model2)
-
-model3 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts + log_likes, data = df_ig)
-summary(model3)
-
-model4 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts + log_comments, data = df_ig)
-summary(model4)
-
-model5 <- lm(change_log_followers ~ log_frac_spon + log_engagement + log_posts + log_likes + log_comments, data = df_ig)
-summary(model5)
-
-stargazer(model, model2, model3, model4, model5, out = 'results.tex')
-
-          
+stargazer(model1, out = paste0(tab, '/results.tex'))
